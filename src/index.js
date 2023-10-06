a71fbb5ae16ec3b1f60fa219f1c0cf4ca609cc01
xjordeen <jordenslater@gmail.com>
2023-10-05 23:12:36 -0700

    adding Javascript and css

---
+++ src/index.js
@@ -0,0 +1,39 @@
+let inputBox = document.getElementById("input");
+let listContainer = document.getElementById("list-container");
+
+function addTasks() {
+  if (inputBox.value === "") {
+    alert("You must write something.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("checked");
+      saveData();
+    } else if (event.target.tagName === "SPAN") {
+      event.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
